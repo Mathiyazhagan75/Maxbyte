da26901897c98c6ffd5f6398a691e7f0e9443673
Mathiyazhagan S <20pw20@psgtech.ac.in>
2022-08-06 20:10:32 +0530

    Added Navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,42 @@
-import React from 'React'
+import React from 'react'
 import "./navbar.scss"
+
 const Navbar = () => {
     return (
         <div className='navbar'>
-            Navbar
+            <div className="nav-title">
+            <h1>CONFIGURATION</h1>
+            </div>
+            <hr class="hr-design"/>
+            <div className="wrapper">
+            <ul>
+                <li>
+                    <span>
+                    SHIFT CONFIGURATION
+                    </span>
+                </li>
+                <li>
+                    <span>
+                    MODEL CONFIGURATION
+                    </span>
+                </li>
+                <li>
+                    <span> 
+                    CHECKLIST CONFIGURATION
+                    </span>
+                </li>
+                <li>
+                    <span> 
+                    MAIL CONFIGURATION
+                    </span>
+                </li>
+                <li>
+                    <span> 
+                    MULTISHIFT CONFIGURATION
+                    </span>
+                </li>
+                </ul>
+            </div>
         </div>
     )
 }
